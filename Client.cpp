5d896df58c94fc5cc22ef14e9ef3ba526ea3b6b3
IntangibleTM <mikeblast22@gmail.com>
2019-05-05 13:31:54 -0700

    modified:   Client.cpp

--- Client.cpp
+++ Client.cpp
@@ -50,7 +50,6 @@ void* pinger(void *t){
         sprintf(buffer, "ping %d", *(td->clientID));
         struct sockaddr_in servaddr; 
 
-        int n, len; 
         // Creating socket file descriptor 
         if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) { 
             printf("socket creation failed"); 
@@ -107,6 +106,114 @@ int registerClientToServer(int socket, string filenames){
     close(socket); 
     return assignedID; 
 }
+//set up get and set ids for the broadcast listener to use
+string id; 
+
+void setClientID(int i)
+{
+    id=to_string(i);
+}
+string getClientID()
+{
+    return id;
+}
+
+//broadcasts the clientID that the client requester wants to find and filename that the client requester wants
+void broadcaster (int id,string fileN) {
+
+	int sock;
+	string broadcastIPs="127.0.0.1";
+	const char *broadcastIP=broadcastIPs.c_str();
+	int broadcastPort=PORT;
+	string clientID = to_string(id);
+    string fileName=fileN;
+	
+	string sendStringS="broad " + clientID +" " + fileName; //what the message is consisted of. broad to know it's for a broadcasting message
+	const char* sendString=sendStringS.c_str();
+
+	unsigned int sendStringLen=sendStringS.length();
+
+    if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+	{
+       printf("socket() failed");
+    }
+	int broadcastPermission;
+	struct sockaddr_in broadcastAddr; 
+
+    /* Set socket to allow broadcast */
+    broadcastPermission = 1;
+    if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, (void *) &broadcastPermission,sizeof(broadcastPermission)) < 0)
+    {
+       printf("setsockeopt() failed");
+       exit(0);
+    }
+
+    /* Construct local address structure */
+    memset(&broadcastAddr, 0, sizeof(broadcastAddr));   /* Zero out structure */
+    broadcastAddr.sin_family = AF_INET;                 /* Internet address family */
+    broadcastAddr.sin_addr.s_addr = inet_addr(broadcastIP);/* Broadcast IP address */
+    broadcastAddr.sin_port = htons(broadcastPort);         /* Broadcast port */
+
+
+    sendto(sock, sendString, sendStringLen, 0, (struct sockaddr *)&broadcastAddr, sizeof(broadcastAddr));
+}
+
+//thread that listens to the broadcast to see if the client themselves need to be messaged about a file request
+void* broadcastListener(void *t)
+{
+	struct thread_data *td;
+	td = (struct thread_data*) t;                      /* Socket */
+    struct sockaddr_in broadcastAddr; /* Broadcast Address */
+    unsigned short broadcastPort=PORT;     /* Port */
+    char recvString[256]; /* Buffer for received string */
+    int recvStringLen;                /* Length of received string */
+
+    /* Construct bind structure */
+    memset(&broadcastAddr, 0, sizeof(broadcastAddr));   /* Zero out structure */
+    broadcastAddr.sin_family = AF_INET;                 /* Internet address family */
+    broadcastAddr.sin_addr.s_addr = htonl(INADDR_ANY);  /* Any incoming interface */
+
+    broadcastAddr.sin_port = htons(broadcastPort);      /* Broadcast port */
+
+    while(1) {
+	if ((td->sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+	{
+		printf("Socket() failed\n");
+	}
+
+	/* Bind to the broadcast port */
+	if (bind(td->sock, (struct sockaddr *) &broadcastAddr, sizeof(broadcastAddr)) < 0)
+	{
+		//printf("bind() failed\n");
+		close(td->sock);
+        continue;
+	}
+
+	/* Receive a single datagram from the server */
+	if ((recvStringLen = recvfrom(td->sock, recvString, 256, 0, NULL, 0)) < 0)
+	{
+		printf("recvfrom() failed");
+	}
+	
+	recvString[recvStringLen] = '\0';
+
+    //breaks down the message and splits them to their respective names for comparison
+    vector<string>* tokens = new vector<string>();
+    string recStr(recvString);
+    separateStringIntoTokens(recStr, tokens);
+    if((tokens->at(0))=="broad")
+    {
+        if(tokens->at(1)==getClientID())
+        {
+            cout<<"Request for " + tokens->at(2)<<endl;
+        }
+    }
+
+    delete tokens;
+	close(td->sock);
+    }
+    exit(0);
+}
 
 //send a filename to search up to the server and the server will respond with what client has that filename. It does this by sending 
 //a message 'filesearch <filename>' and the server responds with 'filesearchresult <ownerIDofFilename>'. If ownerID is -1, it is not found.
@@ -133,6 +240,7 @@ void fileSearch(int socket, string filename){
                     cout << "File is located in Client ID " << ownerID << endl;
 
                     //TODO: BROADCAST FILENAME AND THIS CLIENTID.
+                    broadcaster(ownerID,filename);
 
                 }else{
                     cout << "Unable to find the file." << endl;
@@ -141,19 +249,15 @@ void fileSearch(int socket, string filename){
                 cout << "Unknown command received from server.";
             }
 
-
             delete tokens;
 		}
         close(socket); 
     
 }
 
-
-
 int main(int argc, char const *argv[]) 
 { 
-	struct sockaddr_in address; 
-	int sock = 0, valread; 
+	int sock = 0; 
 	struct sockaddr_in serv_addr;  
 	
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
@@ -178,6 +282,8 @@ int main(int argc, char const *argv[])
     int *clientID = new int();
     //if unassigned, clientID will be -1.
     *(clientID) = -1;
+
+
     
     bool isPublished=false;
     string files;
@@ -190,8 +296,11 @@ int main(int argc, char const *argv[])
     //init thread vars
     pthread_t pingerThread;
 
+    pthread_t broadcastThread;
+
     while(1)
 	{
+
 		int options=0;
 		
 		
@@ -213,10 +322,11 @@ int main(int argc, char const *argv[])
                 
 			cout<<"Registering..."<<endl;
             *clientID = registerClientToServer(sock, files);
-
+            setClientID(*(clientID)); //sets client id for broadcast listener to use
             //start pinging once registered
             int rc2 = pthread_create(&pingerThread, NULL, pinger, (void *)&td);
-             if (rc2) {
+            int rc3 = pthread_create(&broadcastThread, NULL, broadcastListener, (void *)&td);
+             if (rc2||rc3) {
                 printf("Error:unable to create thread\n");
                 exit(-1);
             }
@@ -261,7 +371,7 @@ int main(int argc, char const *argv[])
 		else
 		{
 			cout<<"Exiting"<<endl;
-			break;
+			exit(1);
 		}
 	}
 
@@ -269,6 +379,7 @@ int main(int argc, char const *argv[])
 
 	//end threads when they are done.
     pthread_join(pingerThread, NULL);
+    pthread_join(broadcastThread,NULL);
 
 	pthread_exit(NULL);
 
